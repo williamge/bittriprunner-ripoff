4373328cec0abb0de14c804b62854d6df54b75a3
William Gelhar <williamgelhar@gmail.com>
2015-06-29 02:11:35 -0400

    Adding an autopublish script.

---
+++ autopublish.sh
@@ -0,0 +1,9 @@
+git stash
+git checkout -f gh-pages
+git merge master --strategy-option theirs
+npm run postinstall
+git add bundle.js
+git commit -m "Automatic autopublish"
+git push origin gh-pages
+git checkout master
+git stash pop
